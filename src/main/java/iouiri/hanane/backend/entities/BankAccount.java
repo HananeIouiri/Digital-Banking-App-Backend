9c9bddefba3a358994df44c56ff0cdb6ab51d747
hanane <hananeiouiri1@gmail.com>
2025-05-18 12:46:55 +0100

    Replace the Table per class strategy with the JOINED Table strategy

--- src/main/java/iouiri/hanane/backend/entities/BankAccount.java
+++ src/main/java/iouiri/hanane/backend/entities/BankAccount.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 //@DiscriminatorColumn(name = "TYPE", length = 4)
 @Data @NoArgsConstructor @AllArgsConstructor
 public abstract class BankAccount {
