0ee36a02548a490f1b3a1c2e462d9e5cafc42f69
hanane <hananeiouiri1@gmail.com>
2025-05-18 14:28:33 +0100

    add BankAccountMapperImpl for Customer to CustomerDTO mapping

---
+++ src/main/java/iouiri/hanane/backend/mappers/BankAccountMapperImpl.java
@@ -0,0 +1,23 @@
+package iouiri.hanane.backend.mappers;
+
+
+
+import iouiri.hanane.backend.dtos.CustomerDTO;
+import iouiri.hanane.backend.entities.Customer;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankAccountMapperImpl {
+    public CustomerDTO fromCustomer(Customer customer){
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customer, customerDTO);
+        return customerDTO;
+    }
+
+    public Customer fromCustomerDTO(CustomerDTO customerDTO){
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerDTO, customer);
+        return customer;
+    }
+}
